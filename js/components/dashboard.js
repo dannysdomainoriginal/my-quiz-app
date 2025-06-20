e38203ce3c0677e3f7578642f92cce0972055d98
Charles Daniel <dannysdomainoriginal@gmail.com>
2025-06-20 22:59:24 +0100

    Added quiz modal feature to menu link

--- js/components/dashboard.js
+++ js/components/dashboard.js
@@ -1,25 +1,10 @@
-window.addEventListener('load', () => {
-    let subjects = document.querySelectorAll('.subject .img-box')
-
-    subjects.forEach(subject => {
-        subject.style.backgroundImage = `url('./images/${subject.getAttribute('data-subject')}.png')`
-
-        console.log('done')
-    });
-})
-
-
 // MODAL
 const modal = document.getElementById("quizModal");
-const preGenQuiz = document.getElementById("pre-gen-quiz");
-const aiQuiz = document.getElementById("ai-quiz");
 
 const closeBtn = document.getElementById("closeModalBtn");
 const quizTypeSelect = document.getElementById("quizType");
 const difficultyGroup = document.getElementById("difficultyGroup");
 
-preGenQuiz.onclick = () => modal.classList.add("active");
-aiQuiz.onclick = () => modal.classList.add("active");
 
 closeBtn.onclick = () => modal.classList.remove("active");
 window.onclick = (e) => {
@@ -30,3 +15,59 @@ quizTypeSelect.onchange = () => {
     const value = quizTypeSelect.value;
     difficultyGroup.classList.toggle("hidden", value === "pre");
 };
+
+// Quiz Page
+if(location.href.includes('quiz')){
+    const allLinks = document.querySelectorAll('a')
+
+    allLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault()
+            e.stopPropagation()
+
+            let link = e.currentTarget
+            let target = link.getAttribute('href')
+
+            let confirm = window.confirm('Would you like to quit the current quiz?')
+
+            if(confirm && !link.classList.contains('take-quiz-btn'))location.href = target
+            if(!confirm && link.classList.contains('take-quiz-btn')){
+                setTimeout(() => {
+                    modal.classList.remove('active');
+                }, 300);
+            }
+        })
+    });
+}
+
+// Dashboard Page
+if(location.href.includes('dashboard')) {
+    window.addEventListener('load', () => {
+        let subjects = document.querySelectorAll('.subject .img-box')
+
+        subjects.forEach(subject => {
+            subject.style.backgroundImage = `url('./images/${subject.getAttribute('data-subject')}.png')`
+
+            console.log('done')
+        });
+    })
+ 
+    const preGenQuiz = document.getElementById("pre-gen-quiz");
+    const aiQuiz = document.getElementById("ai-quiz");
+
+    preGenQuiz.onclick = () => modal.classList.add("active");
+    aiQuiz.onclick = () => modal.classList.add("active");
+}
+
+// modal
+const takeQuiz = document.querySelectorAll('.take-quiz-btn')
+takeQuiz.forEach(quizBtn => {
+    quizBtn.addEventListener('click', (e) => {
+        e.stopPropagation()
+        e.preventDefault()
+
+        modal.classList.add("active")
+    })
+});
+
+console.log(takeQuiz)
